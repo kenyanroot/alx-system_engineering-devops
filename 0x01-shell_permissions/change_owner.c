ab8f1c1c145f5c747ca49bdca4b350cd65225131
kenyanroot <ndedaanaco@gmail.com>
2023-08-03 14:57:33 -0700

    possible fix

--- 0x01-shell_permissions/change_owner.c
+++
@@ -1,6 +0,0 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <pwd.h>
-#include <grp.h>
-#include <sys/stat.h>
-int main() { struct stat st; stat("hello", &st); if (st.st_uid == getpwnam("guillaume")->pw_uid) { chown("hello", getpwnam("betty")->pw_uid, -1); } return 0; }
