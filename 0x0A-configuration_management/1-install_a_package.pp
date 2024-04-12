3591c4fe7a75f458998767a32b4fcba9c14e55aa
Anaclate Umudi <46379959+kenyanroot@users.noreply.github.com>
2024-04-13 01:27:23 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,5 @@
-class flask_installation {
-  # Ensure pip3 is present; adjust this accordingly if not already managed elsewhere in your Puppet code
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  # Execute pip3 to install the specific version of Flask
-  exec { 'install-flask':
-    command     => 'pip3 install Flask==2.1.0',
-    unless      => 'pip3 freeze | grep Flask==2.1.0',
-    require     => Package['python3-pip'],
-    path        => ['/bin', '/usr/bin'],
-    environment => 'PATH=/usr/bin:/bin:/usr/local/bin', # Ensure pip3 is in the PATH
-  }
+# installing a package using puppet
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
-
-include flask_installation
