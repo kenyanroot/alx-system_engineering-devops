638c1ec2a56900552059fc6a3e8c22189f51c8fc
Anaclate Umudi <46379959+kenyanroot@users.noreply.github.com>
2024-04-13 01:18:33 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,13 @@
-# install puppet-lint -v 2.5.0
+# Puppet manifest to install Flask version 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Ensure pip3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Ensure Flask is installed using pip3
+exec { 'install-flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
+  require => Package['python3-pip'],
 }
