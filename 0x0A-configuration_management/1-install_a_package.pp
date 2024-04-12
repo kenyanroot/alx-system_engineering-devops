ae4880b4ef242d780ceb9ad2d3d8e1cf759f3de8
Anaclate Umudi <46379959+kenyanroot@users.noreply.github.com>
2024-04-13 01:22:32 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# Puppet manifest to install Flask version 2.1.0
-
-# Ensure pip3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Ensure Flask is installed using pip3
-exec { 'install-flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 freeze | grep Flask==2.1.0',
-  require => Package['python3-pip'],
+# installing a package using puppet
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
